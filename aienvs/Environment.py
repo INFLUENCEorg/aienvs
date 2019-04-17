d0c823f65587722dea91aab05f1fe2e25ba84568
wouter <wouter@tud206796>
2019-04-17 11:42:34 +0200

    #1 add actions to step() and specify how it should be processed

--- aienvs/Environment.py
+++ aienvs/Environment.py
@@ -14,10 +14,17 @@ class Environment(ABC):
         pass
     
     @abstractmethod
-    def step(self) -> EnvironmentState:
+    def step(self, actions: dict) -> EnvironmentState:
         """
         Let the environment make a discrete timestep.
         Can be called only after environment has been initialized.
+        @param actions: a dictionary with actions to be taken in this step.
+        Keys are the entity ids, values are a list of actions to be taken by the entity.
+        Entities may handle more than 1 action every timestep, 
+        this depends on the environment. Environments may throw if illegal actions are taken.
+        The environment is assumed to proceed as follows.
+        1. All entities take the actions as indicated in the actions dictionary
+        2. The simulation advances one timestep.
         @return:  the new EnvironmentState
         """
         pass
