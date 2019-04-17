2e2297c522e3c67e3f1b6ad9c22917777c8f4da0
wouter <wouter@tud206796>
2019-04-17 10:25:36 +0200

    #1 fix setup.py script to include aienvs files

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 import sys, os.path
 
 setup(name='aienvs',
-      version='0.1',
-      description='environments for Influence project.',
-      url='https://github.com/INFLUENCEorg/aienvs',
-      author='Influence TEAM',
-      author_email='author@example.com',
-      license='Example',
-      classifiers=[
+    version='0.1',
+    description='environments for Influence project.',
+    url='https://github.com/INFLUENCEorg/aienvs',
+    author='Influence TEAM',
+    author_email='author@example.com',
+    license='Example',
+    packages=['aienvs'],
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
