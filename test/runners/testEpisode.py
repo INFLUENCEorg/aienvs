c22fe5b1de816d22be200f54e90bddf18c8aa8cc
wouter <wouter@tud206796>
2019-06-20 09:38:49 +0200

    fixed multi-return

--- test/runners/testEpisode.py
+++ test/runners/testEpisode.py
@@ -15,23 +15,18 @@ class testEpisode(LoggedTestCase):
         result = episode.run()
         
         self.assertEqual((1, 3.0), result)
-
-    def testMock(self):
-        env = Mock()
-        env.step = Mock(return_value=[1, 2, 3])
-        print (env.step('a'))
-        print (env.step('b'))
-    
+ 
     def testRun3steps(self):
         agent = Mock()
         env = Mock()
 
-        env.step = Mock(return_value=[('observation1', 3.0, False, {}), ('observation2', 4.0, False, {}), ('observation3', 5.0, True, {})])
+        env.step = Mock()
+        env.step.side_effect = [('observation1', 3.0, False, {}), ('observation2', 4.0, False, {}), ('observation3', 5.0, True, {})]
         firstAction = Mock()
         episode = Episode(agent, env, firstAction, False, 0)
         result = episode.run()
         
-        self.assertEqual((3, 5.0), result)
+        self.assertEqual((3, 12.0), result)
         # if you get not enough values to unpack then 
         # the Episode runner did not halt when env said 
         # it was done.
