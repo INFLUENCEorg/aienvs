38bfe662491310db9ae9a8c80854d4d50364469b
czechows <czechows@users.noreply.github.com>
2019-08-19 20:13:44 +0200

    More bugfixes

--- experiments/pipeline.sh
+++ experiments/pipeline.sh
@@ -3,18 +3,21 @@ unlink data
 
 for (( GEN = 1; GEN <= 5; GEN++ ))      ### Outer for loop ###
 do
-    TJOB=NONE
-    DATADIR = DATA$GEN
-    mkdir $DATADIR
-    DJOBS=()
-    for (( djob = 1 ; djob <= 15; djob++ )) ### Inner for loop ###
-    do
-        DJOBS+=$(./runner.sh $DATADIR $TJOB)
-        DJOBS+=:
-    done
-    DJOBS+=$(./runner.sh $DATADIR $TJOB) # one last one
-    
-    ln -sfn $DATADIR data
-    mv models/robot.h5 models/robot_gen$GEN.h5
-    TJOB=$(sbatch --dependency=afterok:$DJOBS --parsable train_batcher.sh)
+  TJOB=(NONE)
+  DATADIR=DATA$GEN
+  mkdir $DATADIR
+  echo $DATADIR
+  exit 1
+
+  DJOBS=()
+  for (( djob = 1 ; djob <= 15; djob++ )) ### Inner for loop ###
+  do
+      DJOBS+=$(./runner.sh $DATADIR $TJOB)
+      DJOBS+=:
+  done
+  DJOBS+=$(./runner.sh $DATADIR $TJOB) # one last one
+  
+  ln -sfn $DATADIR data
+  mv models/robot.h5 models/robot_gen$GEN.h5
+  TJOB=$(sbatch --dependency=afterok:$DJOBS --parsable train_batcher.sh)
 done
