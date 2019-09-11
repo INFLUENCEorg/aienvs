990f30cff590bf3a6256d247490a6ee50afb1947
wouter <wouter@tud206796>
2019-09-11 16:41:37 +0200

    #127 fixed test that probably was broken already a longer time..

--- test/gym/DecoratedSpaceTest.py
+++ test/gym/DecoratedSpaceTest.py
@@ -25,41 +25,41 @@ class DecoratedSpaceTest(LoggedTestCase):
         space = DecoratedSpace.create(Discrete(5))
         self.assertEquals(5, space.getSize())
         self.assertEquals([], space.getSubSpaces())
-        self.assertEquals(2, space.get(2))
+        self.assertEquals(2, space.getById(2))
 
     def test_Dict(self):
         space = DecoratedSpace.create(Dict({'a':Discrete(5), 'b':Discrete(2)}))
         self.assertEquals(10, space.getSize())
         self.assertEquals(2, len(space.getSubSpaces()))
-        space.get(8)  # smoke test 
-        self.assertEquals({'a':3, 'b':1}, space.get(8))
+        space.getById(8)  # smoke test 
+        self.assertEquals({'a':3, 'b':1}, space.getById(8))
 
     def test_Dict_with_Dict(self):
         space = DecoratedSpace.create(Dict({'p':Dict({'a':Discrete(5), 'b':Discrete(2)}), 'q':Discrete(7)}))
         self.assertEquals(70, space.getSize())
         self.assertEquals(2, len(space.getSubSpaces()))
-        self.assertEquals({'p':{'a':4, 'b':0}, 'q':3}, space.get(34))
+        self.assertEquals({'p':{'a':4, 'b':0}, 'q':3}, space.getById(34))
         
     def test_Box(self):
         space = DecoratedSpace.create(Box(low=-1.0, high=2.0, shape=(3, 4)))
         self.assertEquals(math.inf, space.getSize())
-        self.assertRaises(Exception, space.get, 1)
+        self.assertRaises(Exception, space.getOriginalSpace, 1)
                        
     def test_MultiDiscrete(self):
         space = DecoratedSpace.create(MultiDiscrete([5, 2, 3]))
         self.assertEquals(30, space.getSize())
         self.assertEquals([], space.getSubSpaces())
-        self.assertEquals([2, 1, 2], list(space.get(27)))
+        self.assertEquals([2, 1, 2], list(space.getById(27)))
 
     def test_Tuple(self):
         space = DecoratedSpace.create(Tuple((Discrete(2), Discrete(3))))
         self.assertEquals(2, len(space.getSubSpaces()))
         self.assertEquals(6, space.getSize())
-        self.assertEquals((0, 2), space.get(4))
+        self.assertEquals((0, 2), space.getById(4))
 
     def test_MultiBinary(self):
         space = DecoratedSpace.create(MultiBinary(7))
         self.assertEquals(2 ** 7, space.getSize())
         self.assertEquals([], space.getSubSpaces())
         # reverse of normal binary notation
-        self.assertEquals([0, 1, 0, 1, 1, 0, 1], list(space.get(64 + 16 + 8 + 2)))
+        self.assertEquals([0, 1, 0, 1, 1, 0, 1], list(space.getById(64 + 16 + 8 + 2)))
