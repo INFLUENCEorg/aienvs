fda9cd1be92880746bc2d84e4c458e58784b8afe
wouter <wouter@tud206796>
2019-09-11 15:59:36 +0200

    #121 move part of the FactoryFloor test as integration test to aiagents.

--- test/FactoryFloor/testFactoryFloor.py
+++ test/FactoryFloor/testFactoryFloor.py
@@ -4,11 +4,6 @@ import unittest
 import logging
 from test.LoggedTestCase import LoggedTestCase
 from aienvs.FactoryFloor.FactoryFloor import FactoryFloor
-from aiagents.single.PPO.PPOAgent import PPOAgent
-from aiagents.single.RandomAgent import RandomAgent
-from aiagents.single.mcts.MctsAgent import MctsAgent
-from aiagents.AgentComponent import AgentComponent
-from aiagents.multi.BasicComplexAgent import BasicComplexAgent
 import random
 import yaml
 from numpy import array
@@ -69,63 +64,6 @@ class testFactoryFloor(LoggedTestCase):
         self.assertEquals("[3 1]", str(env._getFreeMapPosition()))
         self.assertEquals("[2 0]", str(env._getFreeMapPosition()))
         self.assertEquals("[8 0]", str(env._getFreeMapPosition()))
-
-    def test_mcts_agent(self):
-        """
-        Really a smoketest/demo how to run an agent
-        """
-        logging.info("Starting test_mcts_agent")
-        dirname = os.path.dirname(__file__)
-        filename = os.path.join(dirname, "../configs/factory_floor_simple.yaml")
-        parameters = getParameters(filename)
-        env = FactoryFloor(parameters)
-        obs = env.reset()
-
-        mctsAgents = []
-
-        randomagent = 'aiagents.single.RandomAgent.RandomAgent'
-        for robotId in env.action_space.spaces.keys():
-            params = {'treeAgent':{'class': randomagent, 'id':robotId, 'parameters':{} },
-                      'rolloutAgent':{'class': randomagent, 'id':robotId, 'parameters':{} }} 
-            mctsAgents.append(MctsAgent(agentId=robotId, environment=env, parameters=params))
-
-        complexAgent = BasicComplexAgent(mctsAgents)
-
-        episode = Episode(complexAgent, env, obs, render=True)
-        episode.run()
-
-    def test_random_agent(self):
-        logging.info("Starting test random agent")
-        dirname = os.path.dirname(__file__)
-        filename = os.path.join(dirname, "../configs/factory_floor_simple.yaml")
-        parameters = getParameters(filename)
-
-        env = FactoryFloor(parameters)
-        randomAgents = []
-        for robotId in env.action_space.spaces.keys():
-            randomAgents.append(RandomAgent(robotId, env))
-
-        complexAgent = BasicComplexAgent(randomAgents)
-        steps = 0
-
-        Experiment(complexAgent, env, 1000, None, True).run()
-
-    def est_PPO_agent(self):
-        logging.info("Starting test PPO agent")
-        dirname = os.path.dirname(__file__)
-        filename = os.path.join(dirname, "../configs/factory_floor_simple.yaml")
-        parameters = getParameters(filename)
-
-        env = FactoryFloor(parameters)
-        PPOAgents = []
-        for robotId in env.action_space.spaces.keys():
-            PPOAgents.append(PPOAgent(parameters, env.observation_space, env.action_space.spaces.get(robotId), robotId))
-
-        complexAgent = BasicComplexAgent(PPOAgents)
-        steps = 0
-
-        while steps < parameters["max_steps"]:
-            steps += Episode(complexAgent, env, env.action_space.sample()).run()
         
 
 if __name__ == '__main__':
