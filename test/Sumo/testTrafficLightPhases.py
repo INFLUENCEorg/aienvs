dfa28821e4a2807bbdfb9842e636da1a1c827cf3
wouter <wouter@tud206796>
2019-09-11 16:55:14 +0200

    #121 fixed file ref so that it works properly regardless of working dir

--- test/Sumo/testTrafficLightPhases.py
+++ test/Sumo/testTrafficLightPhases.py
@@ -2,6 +2,7 @@ from aienvs.Sumo.TrafficLightPhases import TrafficLightPhases
 from test.LoggedTestCase import LoggedTestCase
 import logging
 from io import StringIO
+import os
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -10,7 +11,10 @@ logger.setLevel(logging.INFO)
 class testGym(LoggedTestCase):
         
     def test_load(self):
-        res = TrafficLightPhases('test.tll.xml')
+        dirname = os.path.dirname(__file__)
+        filename = os.path.join(dirname, "test.tll.xml")
+
+        res = TrafficLightPhases(filename)
         self.assertEqual(['0', '1'], res.getIntersectionIds())
         self.assertEqual(1, res.getNrPhases('1'))  # one has yellow
         self.assertEqual(4, res.getNrPhases('0'))  # 4 of 8 have yellow
