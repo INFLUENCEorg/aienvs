899109bea10f8ce7399cde10dfde9d39f3500a35
czechows <czechows@users.noreply.github.com>
2019-10-29 11:22:09 +0100

    changed cluster config

--- experiments/collect_data_batcher.sh
+++ experiments/collect_data_batcher.sh
@@ -6,10 +6,10 @@
 #SBATCH --partition=influence
 
 # The default Quality of Service is the 'short' QoS (maximum run time: 4 hours)
-#SBATCH --qos=short
+#SBATCH --qos=long
 
 # The default run (wall-clock) time is 1 minute
-#SBATCH --time=3:00:00
+#SBATCH --time=6:00:00
 
 # The default number of parallel tasks per job is 1
 #SBATCH --ntasks=1
