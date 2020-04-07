2d893e813e5766a6183ad29bd9cbe107aa815468
wouter <wouter@tud206796>
2020-04-07 14:06:29 +0200

    #34 basic implementation done. Tests needed

--- aienvs/PredatorPrey/PredatorPreyEnv.py
+++ aienvs/PredatorPrey/PredatorPreyEnv.py
@@ -1,5 +1,6 @@
 import logging
 from gym import spaces
+from gym.spaces import Dict, MultiBinary, Tuple
 from aienvs.Environment import Env
 from numpy import set_printoptions, transpose, zeros
 from numpy import array, dstack, ndarray
@@ -16,6 +17,7 @@ from aienvs.BasicMap import BasicMap
 from aienvs.PredatorPrey.PredatorPreyState import PredatorPreyState
 from aienvs.PredatorPrey.Predator import Predator
 from aienvs.PredatorPrey.Prey import Prey
+from typing import List
 
 USE_PossibleActionsSpace = False
 
@@ -52,13 +54,15 @@ class PredatorPreyEnv(Env):
     * predators: a list of dicts. Each dict contains 'id' (unique entity label)
      and 'pos' with list [X,Y] where X,Y is the initial position of the entity
     * map: the BasicMap on which the game is played, see BasicMap.
-    * 
+    * returnRealState: if True, step returns the real state object.
+        if False, step returns a gym state
     """
     DEFAULT_PARAMETERS = {'steps':200,
                 'p':0,
                 'predators':[ {'id': "predator1", 'pos':[3, 4]}, {'id': "predator2", 'pos': 'random'}],  # initial predator positions
                 'preys':[ {'id': "prey1", 'pos':[7, 1]}, {'id': "prey2", 'pos': 'random'}],  # initial prey positions
                 'seed':None,
+                'returnRealState':False,
                 'map':['..........',
                        '..........',
                        '..........',
@@ -71,7 +75,7 @@ class PredatorPreyEnv(Env):
                        '..........']
                 }
 
-    #notice actions 0-4 are same as MovableItemOnMap
+    # notice actions 0-4 are same as MovableItemOnMap
     ACTIONS = {
         0: "UP",
         1: "DOWN",
@@ -87,51 +91,46 @@ class PredatorPreyEnv(Env):
         """
         self._parameters = copy.deepcopy(self.DEFAULT_PARAMETERS)
         self._parameters.update(parameters)
-        self.seed(self._parameters['seed'])
 
+        self.seed(self._parameters['seed'])
         predators = [Predator(p['id'], ndarray(p['pos'], True)) for p in self._parameters['predators']]
         preys = [Prey(p['id'], ndarray(p['pos'], True)) for p in self._parameters['preys']]
         map = BasicMap(self._parameters['map'])
         self._state = PredatorPreyState(predators, preys, map, 0, 0, parameters['steps'])
-
         self._actSpace = spaces.Dict({pred.getId():spaces.Discrete(len(self.ACTIONS)) \
                                       for pred in self._state.getPredators()})
 
     # Override
     def step(self, actions:dict):
-        s=self._state
-        if s.isFinal() or (not actions):
-            return s, s.getReward(), s.isFinal(),[]
-
-        self._stepSurvival(actions)
-        self._stepPredatorActs(actions)
-        self._stepPrey()
+        self._step(actions)
+        s = self._state
+        if self._parameters['returnRealState']:
+            return s, s.getReward(), s.isFinal(), []
+        else:
+            return s.getStateMatrix(), s.getReward(), s.isFinal(), []
+    
+    def _step(self, actions:dict):
+        if(self._state.isFinal()) or (not actions):
+            return 
+        self._catch(actions)
+        self._stepPredators(actions)
+        self._stepPreys()
         self._state = self._state.increment()
-        
-        s=self._state
-        return s, s.getReward(), s.isFinal(),[]
     
     def reset(self):
         self.__init__(self._parameters)
         return copy.deepcopy(self._state)  # should return initial observation
 
-    #Override
+    # Override
     def render(self, delay=0.0, overlay=False):
-        if overlay:
-            import colorama
-            colorama.init()
-
-            def move_cursor(x, y):
-                print ("\x1b[{};{}H".format(y + 1, x + 1))
-
-            def clear():
-                print ("\x1b[2J")
-
-            clear()
-            move_cursor(100, 100)
-            set_printoptions(linewidth=100)
-        bitmap = self._createBitmap()
-        print(transpose(bitmap[:, :, 0] - bitmap[:, :, 1]))
+        map = self._state.getFullMap() # list of strings
+        for y in range(len(map)):
+            for x in range(len(map[y])):
+                if self._state.isPredatorAt(array(x,y)):
+                    map[y][x] = 'P'
+                if (self._state.isPreyAt(array(x,y)):
+                    map[y][x] = '='
+        print(*map, sep="\n")
         time.sleep(delay)
 
     # Override
@@ -153,8 +152,20 @@ class PredatorPreyEnv(Env):
 
     @property
     def observation_space(self):
-        return CustomObjectSpace(self._state);
-
+        """
+        If returnRealSpace=true, the observation is simply the full state.
+        if falsle each entity receives as observation 3 5x5 binary matrices. 
+        Refer to PredatorPreyState#getStateMatrix for details.
+        """
+        if self._parameters['returnRealSpace']:
+            return CustomObjectSpace(self._state);
+        else:
+            mb = MultiBinary(5)  # 5x1 binary matrix
+            mbs = Tuple(mb, mb, mb, mb, mb)  # 5 of them for 5x5 binary
+            entityobs = Tuple(mbs, mbs, mbs)  # an obs is 3 5x5 matrices
+            return Dict ({ pred.getId() : entityobs \
+                           for pred in self._state.getPredators()})
+        
     @property
     def action_space(self):
         if USE_PossibleActionsSpace:
@@ -164,258 +175,55 @@ class PredatorPreyEnv(Env):
             actSpace = self._actSpace
         return actSpace
             
-    ########## Getters ###############################
-    
-    def getMap(self):
+    def getMap(self) -> BasicMap:
         """
         @return: the map of this floor
         """
-        return copy.deepcopy(self._state.getMap())
-
-    def getPart(self, area:ndarray):  # -> FactoryFloor
-        """
-        @param area a numpy array of the form [[xmin,ymin],[xmax,ymax]]. 
-        @return: A new FactoryFloor with same settings as this, but
-        with Map#getPart(area) of this map, and only those bots and tasks that 
-        are in that area. The new factoryfloor is completely independent of this floor.
-        """
-        if(int(self._parameters["N_task_appears"]) > 1):
-            raise Exception("Multiple task appears not supported for getting part of the map")
-
-        parameters = copy.deepcopy(self._parameters)
-        newmap = self._state.getMap().getPart(area)
-        parameters['map'] = newmap.getFullMap()
-        parameters['robots'] = [\
-            { 'id':robot.getId(), 'pos':robot.getPosition().tolist() }\
-            for robot in self._state.robots.values() if self._state.getMap().isInside(robot.getPosition())]
-        parameters['tasks'] = [task.getPosition().tolist() \
-            for task in self._state.tasks if self._state.getMap().isInside(task.getPosition())]
-        parameters['P_task_appears'] = newmap.getTaskProbability()
-        return FactoryFloor(parameters)
-  
-    def isPossible(self, robot:FactoryFloorRobot, action):
-        """
-        @param robot a FactoryFloorRobot
-        @param action (integer) the action to be performed
-        @return: true iff the action will be possible (can succeed) at this point.
-        ACT is considered possible if there is a task at the current position.
-        """
-        pos = robot.getPosition()
-        if action == 0:  # ACT
-            return self._getTask(pos) != None
-        return self._isFree(self._newPos(pos, action))
-        
-    def getPossibleActions(self, robot:FactoryFloorRobot):
-        """
-        @return the possible actions for the given robot on the floor
-        """
-        return [action for action in self.ACTIONS if self.isPossible(robot, action)]
+        return self._state.getMap()
 
     ########## Private functions ##########################
 
-
-    def _stepPrey(self):
+    def _stepPreys(self):
         """
         Let the active preys make their step
         """
         for prey in self._state.getPreys() :
             if prey.isActive():
-                self._state=self._state.withPreyStep(prey, random.choice(range(4)))
-        
+                self._state = self._state.withPreyStep(prey, random.choice(range(4)))
     
-    def _stepPredatorActs(self, actions:dict):
+    def _stepPredators(self, actions:dict):
         """
-        Do the actions of all predators and update state accordingly.
-        It is assumed that all succesful 'catch' actions
-        have been executed already.
+        Do the actions of all active predators and update state accordingly.
+        actions of inactive predators are ignored.
+        It is assumed that all succesful 'catch' actions of predators
+        have been executed already. So predators that are still here
+        and do a 'catch' missed the prey and get penalized.
+        @param actions a dict, keys are predator ids and actions are int ACTIONS.
         """
         for predator in self._state.getPredators() :
             if not predator.isActive():
                 continue
             act = actions[predator.getId()]
-            if ACTIONS[act]=='CATCH':
-                self._state=self._state.withPenalty(self._parameters['p'])
-            else # a step action. 
-                self._state = self._state.withStep(predator, act )
+            if self.ACTIONS[act] == 'CATCH':
+                # note, negative reward.
+                self._state = self._state.withReward(self._parameters['p'])
+            else:  # a step action. 
+                self._state = self._state.withStep(predator, act)
 
-    def _stepSurvival(self, actions:dict):
+    def _catch(self, actions:dict):
         """
-        check survival of all preys. Removes all caught prey and their predators from state
+        Removes all caught prey and their predators from state
         """
-        #Copy the list as we are going to modify state
+        # Copy the list as we are going to modify state
         preys = self._state.getPreys() 
         for prey in preys: 
-            adjacent=self._adjacentPredators(prey.getPosition())
+            adjacent = self._state.getAdjacentPredators(prey.getPosition())
             adjacent = filter(lambda adj: self.ACTIONS[actions[adj]] == 'CATCH', adjacent)
-            if len(adjacent)>=2:
+            if len(adjacent) >= 2:
                 # remove prey and first two that catch it 
                 self._state = self._state.withCatch(prey, adjacent[0], adjacent[1]) 
-        
     
-    
-    def _adjacentPredators(self, pos:ndarray) -> List[Predator]:
-        """
-        @param pos a ndarray with some [x,y] position on the map
-        @return all active predators that are directly N,E,S or W from given pos
-        """
-        return filter(lambda pred: pred.isAdjacent(pos) and pred.isActive(), \
-                       self._state.getPredators() )
 
-    def _createBitmap(self):
-        map = self._state.getMap()
-        bitmapRobots = zeros((map.getWidth(), map.getHeight()))
-        bitmapTasks = zeros((map.getWidth(), map.getHeight()))
-        for robot in self._state.robots.values():
-            pos = robot.getPosition()
-            bitmapRobots[pos[0], pos[1]] += 1
+   
 
-        for task in self._state.tasks:
-            pos = task.getPosition()
-            bitmapTasks[pos[0], pos[1]] += 1
-
-        return dstack((9 * bitmapRobots, bitmapTasks))
-
-    def _applyAction(self, pred:Predator, action) -> PredatorPreyState:
-        """
-        predator tries to execute given action.
-        @param robot a FactoryFloorRobot
-        @param action the ACTION number. 
-        """
-        actstring = self.ACTIONS.get(action)
-        randNo = random.random()
-        try:
-            # first check for individual success probabilities
-            pSucceed = self._parameters['P_action_succeed'][pred.getId()][actstring]
-        except KeyError:
-            # then use common ones
-            pSucceed = self._parameters['P_action_succeed'][actstring]
-
-        if randNo > pSucceed:
-            return False
-
-        pos = robot.getPosition()
-        
-        if actstring == "ACT":
-            newpos = pos
-            task = self._getTask(newpos)
-            if task != None:
-                self._state.tasks.remove(task)
-                # logging.debug("removed " + str(task))
-        else:  # move
-            newpos = self._newPos(pos, action)
-            if self._isFree(newpos):
-                robot.setPosition(newpos)
-  
-    def _newPos(self, pos:ndarray, action):
-        """
-        @param pos the current (old) position of the robot (numpy array)
-        @param action the action to be done in given position
-        @return:  what would be the new position (ndarray) if robot did action.
-        This does not check any legality of the new position, so the 
-        position may run off the map or on a wall.
-        """
-        newpos = pos
-        if self.ACTIONS.get(action) == "DOWN":
-            newpos = pos + [0, 1]
-        elif self.ACTIONS.get(action) == "RIGHT":
-            newpos = pos + [1, 0]
-        elif self.ACTIONS.get(action) == "UP":
-            newpos = pos + [0, -1]
-        elif self.ACTIONS.get(action) == "LEFT":
-            newpos = pos + [-1, 0]
-        return newpos
-    
-    def _getFreeMapPosition(self):
-        """
-        @return:random map position (x,y) that is not occupied by robot or wall.
-        """
-        while True:
-            pos = self._state.getMap().getRandomPosition()
-            if self._isFree(pos):
-                return pos
-
-    def _isRobot(self, position:ndarray):
-        """
-        @param position a numpy ndarray [x,y] that must be checked
-        @return: true iff a robot occupies position
-        """        
-        for robot in self._state.robots.values():
-            if (position == robot.getPosition()).all():
-                return True
-        return False
-
-    def _isFree(self, pos:ndarray):
-        """
-        @return true iff the given pos has space for a robot,
-        so it must be on the map and not on a wall and possibly
-        not already containing a robot
-        """
-        map = self._state.getMap()
-        return map.isInside(pos) and map.get(pos) != "*" \
-            and (self._parameters['allow_robot_overlap'] or not(self._isRobot(pos)))
-        
-    def _addTask(self):
-        """
-        Add one new task to the task pool
-        """
-        themap = self._state.getMap()
-        poslist = list(themap.getTaskPositions())
-        if not self._parameters['allow_task_overlap']:
-            if len(self._state.tasks) >= len(poslist):
-                return
-
-        # samplingSpace = spaces.MultiDiscrete([self._parameters['x_size'], self._parameters['y_size']])
-        while True:  # do until newpos is not yet tasked, or task overlap allowed
-            # work around numpy bug when list contains tuples
-            weights = list(themap.getTaskWeights())
-            i = weightedchoice(list(range(len(poslist))), 1, p=weights)[0]
-            newpos = poslist[i]
-            if self._parameters['allow_task_overlap'] or self._getTask(newpos) == None:
-                break;
-
-        self._state.addTask(FactoryFloorTask(newpos))
-
-    def _getTask(self, pos:tuple):
-        """
-        @return task at given position, or None if no task at position
-        """
-        for task in self._state.tasks:
-            if (task.getPosition() == pos).all():
-                return task
-        return None
-
-    def _computePenalty(self):
-        penalty = 0
-        for task in self._state.tasks:
-            penalty += 1
-        return penalty
-
-
-class PossibleActionsSpace(FixedActionsSpace):
-    """
-    A gym space that returns the possible actions at this moment
-    on the factory floor for some robot.
-    REQUIREMENT: at all times, at least one action must be possible.
-    If not, sample() may raise an exception
-    NOTE this class is very tightly coupled to FactoryFloor.
-    @param thefloor: the FactoryFloor
-    @param bot: the FactoryFloorRobot
-    """
-
-    def __init__(self, fl:FactoryFloor, bot:FactoryFloorRobot):
-        super().__init__()
-        self._floor = fl
-        self._robot = bot
-    
-    # Override
-    def sample(self):
-        return random.choice(self._floor.getPossibleActions(self._robot))
- 
-    # Override       
-    def contains(self, act):
-        return self._floor.isPossible(self._robot, act)
-    
-    # Override
-    def getAllActions(self) -> dict:
-        return self._floor.ACTIONS
     
--- aienvs/PredatorPrey/PredatorPreyState.py
+++ aienvs/PredatorPrey/PredatorPreyState.py
@@ -1,8 +1,8 @@
-import numpy as np
-from aienvs.FactoryFloor.Map import Map
+from numpy import array, ndarray, array_equal
 from typing import List
 from aienvs.PredatorPrey.Predator import Predator
 from aienvs.PredatorPrey.Prey import Prey
+from aienvs.BasicMap import BasicMap
 
 
 class PredatorPreyState():
@@ -10,9 +10,12 @@ class PredatorPreyState():
     Stores the entire state of the PredatorPreyEnv:
     the map, the positions of the entities and preys,
     current total reward etc. immutable
+    
+    NOTICE the equals function uses ALL fields and therefore 
+    might not be appropriate for learning.
     """
 
-    def __init__(self, predatorsList:List[Predator], preyList:List[Prey], themap:Map, r:float, s:int, maxs: int):
+    def __init__(self, predatorsList:List[Predator], preyList:List[Prey], themap:BasicMap, r:float, s:int, maxs: int):
         """
         @param predatorsList: list of PredatorEntity objects, each having a unique id.
         @param preyList: list of Prey objects, each having a unique id.
@@ -37,9 +40,38 @@ class PredatorPreyState():
     def getPreys(self):
         return self._preys.copy()
     
+    def getObservationMatrix(self):
+        """
+        @return a gym-style dict with for each entity (predator) 
+        a 3x5x5 matrix, containing the observations for all predators. 
+        See _getPredatorObsMatrix for more details on each 3x5x5 matrix
+        """
+        return { pred.getId(): self._getPredatorObsMatrix(pred)\
+                for pred in self._predators}
+    
+    def isPredatorAt(self, pos:ndarray):
+        """
+        @return true iff there is a predator at given position
+        """
+        return any(map(lambda pred: array_equal(pred.getPosition(), pos)), self._predators)
+
+    def isPreyAt(self, pos:ndarray):
+        """
+        @return true iff there is a prey at given position
+        """
+        return any(map(lambda prey: array_equal(prey.getPosition(), pos)), self._preys)
+
+    def getAdjacentPredators(self, pos:ndarray) -> List[Predator]:
+        """
+        @param pos a ndarray with some [x,y] position on the map
+        @return all active predators that are directly N,E,S or W from given pos
+        """
+        return filter(lambda pred: pred.isAdjacent(pos) and pred.isActive(), \
+                       self.getPredators())
+
     def withCatch(self, prey:Prey, catcher1:Predator, catcher2:Predator) -> PredatorPreyState:
         """
-        Execute a catch. 
+        Execute a catch. It is assumed that all are correctly placed to do this.
         @param prey the prey that was caught
         @param catcher1 the first Predator that did the catch
         @param catcher2 the second Predator that did the catch
@@ -56,21 +88,87 @@ class PredatorPreyState():
         return PredatorPreyState(newpreds, newpreys, self._map, \
             self._reward + 10, self._step, self._maxsteps)
         
-    def increment(self) -> PredatorPreyState:
+    def withReward(self, reward:float) -> PredatorPreyState:
+        """
+        Adds given reward 
+        """
         if self.isFinal():
             return self
+        return PredatorPreyState(self._predators, self._preys, self._map, \
+            self._reward + reward, self._step, self._maxsteps) 
+        
+    def withStep(self, pred: Predator, act: int):
+        """
+        Predator does a move action
+        @act an action in [0,3] moving N,E,S or W. 
+        """
+        preds = self.getPredators()
+        preds[pred.getId()] = pred.withStep(act)
+        return PredatorPreyState(preds, preds, self._map, \
+            self._reward, self._step, self._maxsteps) 
+
+    def withPreyStep(self, prey: Prey, act: int):
+        """
+        Prey does a move action
+        @act an action in [0,3] moving N,E,S or W. 
+        """
+        preys = self.getPreys()
+        preys[prey.getId()] = prey.withStep(act)
+        return PredatorPreyState(self._predators, preys, self._map, \
+            self._reward, self._step, self._maxsteps) 
+        
+    def increment(self) -> PredatorPreyState:
         """
         Increment the step counter
+        @return new PredatorPreyState with incremented step counter.
         """
+        if self.isFinal():
+            return self
         return PredatorPreyState(self._predators, self._preys, self._map, \
                                  self._reward, self._step + 1, self._maxsteps)
     
     def isFinal(self):
         """
-        returns true if all entities have been removed or after STEPS time steps. 
+        @return true if all entities have been removed or after STEPS time steps.
+        NOTICE this is according to the paper. BUT if only 1 entity is left,
+        there is nothing he can do. 
         """
         activePredator = any([predator.isActive() for predator in self._predators])
-        return (not activePredator) or self._s >= self._maxs
+        return (not activePredator) or self._step >= self._maxsteps
+
+    def _getPredatorObsMatrix(self, pred:Predator):
+        """
+        @param pred the predator to get obs matrix for
+        @return the observation matrix (3x5x5) of given predator.
+        Each 5x5 matrix represents the 5x5 area around the entity, in 
+        the top-left to bottom-right. So first line in the matrix
+        is the NW to NE content. The last row the SW to SE content.
+        The center position in the matrix (3,3) represents the 
+        position of the predator himself.
+        The first matrix contains a 1 at positions where there is a
+        predator on the map.
+        The second matrix contains 1 at positions where there is a 
+        prey on the map
+        The third matrix contains 1 at positions where there is an 
+        obstacle on the map, eg a wall or edge of the map.
+        The full obs space is a dict with a 3x5x5 matrix for each predator. 
+
+        """
+        predmap = [ [0] * 5 for row in range(5)]
+        preymap = [ [0] * 5 for row in range(5)]
+        obstmap = [ [0] * 5 for row in range(5)]
+        mypos = pred.getPosition()
+        for x in range(0, 5):
+            for y in range(0, 5):
+                pos = array([x - 2, y - 2]) + mypos
+                if self._map.isFree(pos):
+                    obstmap[y][x] = 1
+                if self.isPredatorAt(pos):
+                    predmap[y][x] = 1
+                if self.isPreyAt(pos):
+                    preymap[y][x] = 1
+                    
+        return [predmap, preymap, obstmap]
 
     def __eq__(self, other):
         return self._predators == other._predators \
