55d43091e3ebd034725d4b52eeca9e71d1c19075
wouter <wouter@tud206796>
2020-06-03 13:19:46 +0200

    #43 trying reproduce issue

--- test/testSumoGymAdapter.py
+++ test/testSumoGymAdapter.py
@@ -17,24 +17,6 @@ class testSumoGymAdapter(LoggedTestCase):
     aiagents project must be installed 
     """
 
-    def test_new_traffic(self):
-        logging.info("Starting test_new_traffic")
-        dirname = os.path.dirname(__file__)
-        filename = os.path.join(dirname, "configs/new_traffic_loop_ppo.yaml")
-
-        with open(filename, 'r') as stream:
-            try:
-                parameters = yaml.safe_load(stream)['parameters']
-            except yaml.YAMLError as exc:
-                print(exc)
-
-        env = SumoGymAdapter(parameters)
-        env.reset()
-
-        for i in range(1000):
-            logging.debug("Step " + str(i))
-            result = env.step(env.action_space.sample())
-        
     def test_smoke(self):
         logging.info("Starting test_smoke")
         env = SumoGymAdapter(parameters={'generate_conf':False, 'gui': False, 'maxConnectRetries':2})
@@ -47,6 +29,40 @@ class testSumoGymAdapter(LoggedTestCase):
             logging.debug("Step " + str(i))
             i += 1
             obs, global_reward, done, info = env.step(env.action_space.sample())
+ 
+    def test_new_traffic(self):
+        self._runPpoLoop()
+
+    def test_runTwice(self):
+        """
+        Tests that we can open SumoGymAdapter twice #43.
+        """
+        self._runPpoLoop()
+        self._runPpoLoop()
+    
+    def _runPpoLoop(self):
+        parameters = self._get_params()
+        env = SumoGymAdapter(parameters)
+        env.reset()
+
+        for i in range(1000):
+            logging.debug("Step " + str(i))
+            result = env.step(env.action_space.sample())
+        # env.close() #DO WE NEED THIS?
+    
+    def _get_params(self) -> dict:
+        """
+        @return new_traffic_loop_ppo parameters
+        """
+        dirname = os.path.dirname(__file__)
+        filename = os.path.join(dirname, "configs/new_traffic_loop_ppo.yaml")
+
+        with open(filename, 'r') as stream:
+            try:
+                parameters = yaml.safe_load(stream)['parameters']
+            except yaml.YAMLError as exc:
+                print(exc)
+        return parameters
  
         
 if __name__ == '__main__':
